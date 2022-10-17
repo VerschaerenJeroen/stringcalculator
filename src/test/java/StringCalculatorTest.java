d1e2f5edc5928f7ed794f2663cc01614eac27b46
Jeroen Verschaeren <jeroen.verschaeren@bignited.be>
2022-10-17 15:39:32 +0200

    added more testcases

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -16,4 +16,22 @@ public class StringCalculatorTest {
         StringCalculator calculator = new StringCalculator("1");
         assertEquals(1, calculator.calculate());
     }
+
+    @Test
+    public void seperatorStringInput() {
+        StringCalculator calculator = new StringCalculator("1,2");
+        assertEquals(3, calculator.calculate());
+    }
+
+    @Test(expected = Exception.class)
+    public void negativeNumberStringInput() throws Exception {
+        StringCalculator calculator = new StringCalculator("1,-2");
+        calculator.calculate();
+    }
+
+    @Test
+    public void numberBiggerThan1000StringInput() {
+        StringCalculator calculator = new StringCalculator("1,1000,2");
+        assertEquals(3, calculator.calculate());
+    }
 }
