0a1a71b3ccc597852ed79254a3f5b2e3a6baee12
Jeroen Verschaeren <jeroen.verschaeren@bignited.be>
2022-10-17 16:13:57 +0200

    When two numbers or more are added to string

--- src/main/java/bignited/StringCalculator.java
+++ src/main/java/bignited/StringCalculator.java
@@ -2,15 +2,40 @@ package bignited;
 
 public class StringCalculator {
 
-    String numberList;
+    private final String numberList;
+    private String seperator = ",";
+    private String numbersWithSeperator;
+
     public StringCalculator(String numberList) {
         this.numberList = numberList;
     }
 
-    public int calculate() {
-        if (numberList.length() == 1) {
-            return Character.getNumericValue(numberList.charAt(0));
+    public int add() {
+        if (isInputEmpty()) {
+            return 0;
         }
-        return 0;
+        numbersWithSeperator = numberList;
+        String[] inputSplittedBySeperator = splitInputBySeperator();
+        return calculateSum(inputSplittedBySeperator);
+    }
+
+    private int calculateSum(String[] inputSplittedBySeperator) {
+        int result = 0;
+        for (String number : inputSplittedBySeperator) {
+            result += addSingleNumber(number);
+        }
+        return result;
+    }
+
+    private int addSingleNumber(String number) {
+        return Integer.parseInt(number);
+    }
+
+    private String[] splitInputBySeperator() {
+        return numbersWithSeperator.split(seperator);
+    }
+
+    private boolean isInputEmpty() {
+        return numberList.length() == 0;
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -8,30 +8,30 @@ public class StringCalculatorTest {
     @Test
     public void emptyStringInput() {
         StringCalculator calculator = new StringCalculator("");
-        assertEquals(0, calculator.calculate());
+        assertEquals(0, calculator.add());
     }
 
     @Test
     public void oneNumberStringInput() {
         StringCalculator calculator = new StringCalculator("1");
-        assertEquals(1, calculator.calculate());
+        assertEquals(1, calculator.add());
     }
 
     @Test
     public void seperatorStringInput() {
         StringCalculator calculator = new StringCalculator("1,2");
-        assertEquals(3, calculator.calculate());
+        assertEquals(3, calculator.add());
     }
 
     @Test(expected = Exception.class)
     public void negativeNumberStringInput() throws Exception {
         StringCalculator calculator = new StringCalculator("1,-2");
-        calculator.calculate();
+        calculator.add();
     }
 
     @Test
     public void numberBiggerThan1000StringInput() {
         StringCalculator calculator = new StringCalculator("1,1000,2");
-        assertEquals(3, calculator.calculate());
+        assertEquals(3, calculator.add());
     }
 }
